ec8ae626fcd20b99188689c97817f7b93f4b5024
DESKTOP-Q3EC6C7\gaomingyang <gaomingyang@migu.com.cn>
2022-08-03 15:34:26 +0800

    适配新电脑，Android 3.5，Android12.

--- MyApplication/app/src/main/java/gaomingyang/myapplication/utils/Userutils.java
+++ MyApplication/app/src/main/java/gaomingyang/myapplication/utils/Userutils.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.blankj.utilcode.util.EncryptUtils;
 import com.blankj.utilcode.util.RegexUtils;
@@ -19,6 +20,7 @@ import gaomingyang.myapplication.Model.UserModel;
 import gaomingyang.myapplication.R;
 
 public class Userutils {
+    private final static String TAG = "Userutils";
     /**
      * 验证登录用户输入的合法性
      */
@@ -28,10 +30,12 @@ public class Userutils {
         //精确的
           if (!RegexUtils.isMobileExact(phone)) {
               ToastUtils.showShort("无效手机号!");
+              Log.e(TAG,"无效手机号!");
               return false;
           }
           if (TextUtils.isEmpty(password)){
               ToastUtils.showShort("密码不能为空!");
+              Log.e(TAG,"密码不能为空!");
               return false;
           }
 
@@ -41,6 +45,7 @@ public class Userutils {
          */
         if (!Userutils.userExistFromPhone(phone)){
             ToastUtils.showShort("当前手机号未注册!");
+            Log.e(TAG,"当前手机号未注册!");
             return false;
         }
         RealmHelp realmHelp = new RealmHelp();
@@ -48,6 +53,7 @@ public class Userutils {
         realmHelp.closeRealm();
         if (result == false){
             ToastUtils.showShort("手机号或者密码不正确!");
+            Log.e(TAG,"手机号或者密码不正确!");
             return false;
         }
 
@@ -55,6 +61,7 @@ public class Userutils {
        boolean isSave =   SPUtils.saveUser(context,phone);
         if (!isSave){
             ToastUtils.showShort("系统错误，请稍后重试!");
+            Log.e(TAG,"系统错误，请稍后重试!");
             return false;
         }
         //保存用户标记,在全局单例类中
@@ -71,6 +78,7 @@ public class Userutils {
         boolean isRemove = SPUtils.removeUser(context);
         if (!isRemove){
             ToastUtils.showShort("系统错误，请稍后重试!");
+            Log.e(TAG,"系统错误，请稍后重试!");
         }
         Intent intent = new Intent(context,LoginActivity.class);
         //清理task栈，并且新生成一个task栈
@@ -90,15 +98,18 @@ public class Userutils {
     public  static  boolean registerUser(Context context,String phone,String password,String passwordConfirm){
         if (!RegexUtils.isMobileExact(phone)) {
             ToastUtils.showShort("无效手机号!");
+            Log.e(TAG,"无效手机号!");
             return false;
         }
         if (StringUtils.isEmpty(password) || !password.equals(passwordConfirm)){
             ToastUtils.showShort("请确认密码!");
+            Log.e(TAG,"请确认密码!");
             return false;
         }
         //用户当前输入的手机号是否已经被注册了
         if (Userutils.userExistFromPhone(phone)){
             ToastUtils.showShort("该手机号已存在!");
+            Log.e(TAG,"该手机号已存在!");
             return false;
         }
 
@@ -158,14 +169,17 @@ public class Userutils {
          */
         if (TextUtils.isEmpty(oldPassword)){
             ToastUtils.showShort("请输入原密码!");
+            Log.e(TAG,"请输入原密码!");
             return  false;
         }
         if (TextUtils.isEmpty(newPassword)){
             ToastUtils.showShort("请输入新密码!");
+            Log.e(TAG,"请输入新密码!");
             return  false;
         }
         if (!newPassword.equals(newPasswordConfirm)){
             ToastUtils.showShort("两次输入的新密码不一致!");
+            Log.e(TAG,"两次输入的新密码不一致!");
             return  false;
         }
         RealmHelp realmHelp =  new RealmHelp();
@@ -173,6 +187,7 @@ public class Userutils {
         if (userModel != null){
             if (!EncryptUtils.encryptMD5ToString(oldPassword).equals(userModel.getPassword())){
                 ToastUtils.showShort("原密码不正确!");
+                Log.e(TAG,"原密码不正确!");
                 realmHelp.closeRealm();
                 return false;
             }else {
